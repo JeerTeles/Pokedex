1554d85328c88012520ae132a646821096a007ef
JeerTeles <jadersoncontatos@gmail.com>
2023-12-11 01:37:33 -0300

    Trans formando a lista de pokemon  na lista HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,36 @@ const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}` /* pegando a url da api */
 
 
+function convertPokemonToLi(pokemon){
+    return ` 
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poisson</li>
+                </ol>
+
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
 /* Arrow funtion */
 fetch(url)
     .then((response) => response.json()) /* quando o fecth de certo chame a função response (body convertido em json) */
-    .then((jsonBody) => console.log(jsonBody)) /* printando o body */
-    .catch((error) => console.error(error))
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToLi(pokemon);
+        }
+    })
+    .catch((error) => console.error(error));
 
 
