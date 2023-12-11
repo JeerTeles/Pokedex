2020b83207c1095f48b4a2f3dd7b7c013bfccdae
JeerTeles <jadersoncontatos@gmail.com>
2023-12-10 22:15:16 -0300

    Criando a Arrow function para APi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-console.log('Sucesso!');
+
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}` /* pegando a url da api */
+
+
+/* Arrow funtion */
+fetch(url)
+    .then((response) => response.json()) /* quando o fecth de certo chame a função response (body convertido em json) */
+    .then((jsonBody) => console.log(jsonBody)) /* printando o body */
+    .catch((error) => console.error(error))
+
+
