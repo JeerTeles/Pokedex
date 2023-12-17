f01750e752b5258721b26849b749a80b39097a21
JeerTeles <jadersoncontatos@gmail.com>
2023-12-17 02:24:58 -0300

    Mudando o modelo da PokeAPi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,15 @@
 
-function convertPokemonTypesToLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
-
 function convertPokemonToLi(pokemon){
     return ` 
         <li class="pokemon">
-            <span class="number">#${pokemon.order}</span>
+            <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
             <div class="detail">
                 <ol class="types">
-                    ${convertPokemonTypesToLi(pokemon.types).join('')}
+                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                 </ol>
 
-                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
             </div>
         </li>
     `
@@ -24,7 +20,8 @@ const pokemonList = document.getElementById('pokemonList');
 /* Arrow funtion */
     
 pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml
   
     /*const listItems =[];
     for (let i = 0; i < pokemons.length; i++) {
